31807f7d7d66c281f7f470c515d345d72e828a83
Yoo Hyungjun <70789958+HyungJun-Yoo@users.noreply.github.com>
2023-05-11 02:36:43 +0900

    [Bronze II] Title: 단어의 개수, Time: 152 ms, Memory: 17868 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -3,5 +3,5 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n')
 
 const result = input[0].trim().split(' ')
 
-if (result == '') console.log(0)
+if (result[0] === '') console.log(0)
 else console.log(result.length)
