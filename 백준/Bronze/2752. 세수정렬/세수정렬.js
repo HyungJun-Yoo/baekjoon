cf132f75b27b655d9d87f31f65cefb43d1a424dc
Yoo Hyungjun <70789958+HyungJun-Yoo@users.noreply.github.com>
2023-05-15 20:35:58 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const result = input[0].split(' ').map(Number)
+
+result.sort((a, b) => a - b)
+
+console.log(result.join(' '))
