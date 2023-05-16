ebe28475a850dc59c99be9a84d62c543c42deb6c
Yoo Hyungjun <70789958+HyungJun-Yoo@users.noreply.github.com>
2023-05-16 13:33:03 +0900

    [Silver IV] Title: 동전 0, Time: 440 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let [n, k] = input[0].split(' ').map(Number)
+
+// 동전 담기
+let money = []
+for (let i = 1; i <= n; i++) {
+  money.push(Number(input[i]))
+}
+
+// K원을 만드는데 필요한 동전 개수 구하기
+let count = 0
+while (k >= money[0]) {
+  for (let i = money.length - 1; i >= 0 ;i--) {
+    if (k >= money[i]) {
+      k -= money[i]
+      count++
+      break
+    }
+  }
+}
+
+console.log(count)
