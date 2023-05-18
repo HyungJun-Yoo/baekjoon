b435875c9cbdc0bafc116e766442c2f1e5004085
Yoo Hyungjun <70789958+HyungJun-Yoo@users.noreply.github.com>
2023-05-19 07:33:29 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,31 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let [n, m] = input[0].split(' ').map(Number)
+let arr = []
+
+function possible(num) {
+  for (let x of arr) {
+    if (x === num) return false
+  }
+
+  return true
+}
+
+let result = ''
+function dfs(num) {
+  if (num > m) {
+    result += `${arr.join(' ')} \n`
+    return
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!possible(i)) continue
+    arr.push(i)
+    dfs(num + 1)
+    arr.pop()
+  }
+}
+
+dfs(1)
+console.log(result)
